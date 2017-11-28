8d7a63b1b4c8effdb4e4e7e017b07c87688d0f62
David Spader <davidspader97@gmail.com>
2017-11-28 11:16:40 -0200

    Alterando a página inicial

--- home.php
+++ home.php
@@ -38,7 +38,7 @@
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse1" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <a class="navbar-brand" href="#"><img src="landing-page/img/logo/logo-verde.png"</span></a>
+                <a class="navbar-brand" href="index.php"><img src="css/imagens/logo/logo-verde.png"</span></a>
                 <div class="collapse navbar-collapse" id="navbarCollapse1">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item"> <a class="nav-link" href="index.php?pagina=login">Cadastre-se</a> </li>
@@ -55,10 +55,10 @@
         <div class="swiper-container main-slider" id="myCarousel">
             <div class="swiper-wrapper">
                 <div class="swiper-slide slider-bg-position" style="background:url('landing-page/img/1.jpg')" data-hash="slide1">
-                    <h2>It is health that is real wealth and not pieces of gold and silver</h2>
+                    <h2>Tenha total controle sobre suas reservas!</h2>
                 </div>
-                <div class="swiper-slide slider-bg-position" style="background:url('landing-page/img/3.jpg')" data-hash="slide2">
-                    <h2>Happiness is nothing more than good health and a bad memory</h2>
+                <div class="swiper-slide slider-bg-position" style="background:url('landing-page/img/2.jpg')" data-hash="slide2">
+                    <h2>Cadastre-se agora, e não perca essa oportunidade!</h2>
                 </div>
             </div>
             <!-- Add Pagination -->
@@ -68,58 +68,56 @@
             <div class="swiper-button-next"><i class="fa fa-chevron-right"></i></div>
         </div>
 
-        <!-- Benefits
-            ================================================== -->
-        <section class="service-sec" id="benefits">
+        <section class="service-sec">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
                         <div class="heading text-md-center text-xs-center">
-                            <h2><small>Benefits of Exercise</small>To enjoy the glow of good health, you must exercise</h2>
+                            <h2>Características do sistema:</h2>
                         </div>
                     </div>
-                    <div class="col-md-12">
+                    <div>
                         <div class="row">
-                            <div class="col-md-3 text-sm-center service-block"> <i class="fa fa-plus" aria-hidden="true"></i>
-                                <h3>Better Sleep</h3>
-                                <p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe.</p>
+                            <div class="col-md-3 text-sm-center service-block"> <i class="fa fa-home" aria-hidden="true"></i>
+                                <h3>Imóveis</h3>
+                                <h3><small>Cadastre seus <br>imóveis!</small></h3>
+                                <p>Seus imóveis podem ser casas ou apartamentos, e nós também permitimos que você adicione o endereço de cada imóvel. Todos os imóveis são gerenciados individualmente.</p>
                             </div>
-                            <div class="col-md-3 text-sm-center service-block"> <i class="fa fa-leaf" aria-hidden="true"></i>
-                                <h3>Reduces Weight</h3>
-                                <p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe.</p>
+                            <div class="col-md-3 text-sm-center service-block"> <i class="fa fa-user" aria-hidden="true"></i>
+                                <h3>Intermediadores</h3>
+                                <h3><small>Cadastre seus <br>intermediadores!</small></h3>
+                                <p>Seus intermediadores podem ser pessoas físicas ou jurídicas que auxiliam na locação do imóvel e nós também permitimos que você adicione os dados básicos da pessoa.</p>
                             </div>
-                            <div class="col-md-3 text-sm-center service-block"> <i class="fa fa-leaf" aria-hidden="true"></i>
-                                <h3>Improves Mood</h3>
-                                <p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe.</p>
+                            <div class="col-md-3 text-sm-center service-block"> <i class="fa fa-calendar-o" aria-hidden="true"></i>
+                                <h3>Reservas</h3>
+                                <h3><small>Cadastre suas <br>reservas!</small></h3>
+                                <p>Suas reservas serão criadas a partir dos seus imóveis, nós permitimos que você adicione todos os dados relevantes em uma reserva e visualize-a com facilidade.</p>
                             </div>
-                            <div class="col-md-3 text-sm-center service-block"> <i class="fa fa-bell" aria-hidden="true"></i>
-                                <h3>Boosts Energy</h3>
-                                <p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe.</p>
+                            <div class="col-md-3 text-sm-center service-block"> <i class="fa fa-handshake-o" aria-hidden="true"></i>
+                                <h3>Contratos</h3>
+                                <h3><small>Crie seus <br>contratos!</small></h3>
+                                <p>Seus contratos são gerados de forma dinâmica com base nas informações que você adicionou no seu imóvel, intermediador e reserva e informará apenas os dados do locatário.</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <!-- /.row --> 
             </div>
         </section>
 
-        <!-- About 
-            ================================================== -->
         <section class="about-sec parallax-section" id="about">
             <div class="container">
                 <div class="row">
                     <div class="col-md-3">
-                        <h2><small>Who We Are</small>About<br>
-                            Our Blog</h2>
+                        <h2>Sobre o projeto</h2>
                     </div>
                     <div class="col-md-4">
-                        <p>To enjoy good health, to bring true happiness to one's family, to bring peace to all, one must first discipline and control one's own mind. If a man can control his mind he can find the way to Enlightenment, and all wisdom and virtue will naturally come to him.</p>
-                        <p>Saving our planet, lifting people out of poverty, advancing economic growth... these are one and the same fight. We must connect the dots between climate change, water scarcity, energy shortages, global health, food security and women's empowerment. Solutions to one problem must be solutions for all.</p>
+                        <p>O projeto Suas Reservas consiste em um sistema de gerenciamento de reservas em aluguéis de temporada, com foco no proprietário do imóvel, onde o mesmo poderá ter controle sobre suas reservas.</p>
+                        <p>Existem vários sistemas para imobiliárias, hotéis e pousadas, porém eles não se adaptam bem a pequenos empreendedores que alugam suas próprias casas na temporada, pois além de serem formas diferentes de aluguel, os pequenos empreendedores muitas vezes não precisam de sistemas complexos.</p>
                     </div>
                     <div class="col-md-4">
-                        <p>Our greatest happiness does not depend on the condition of life in which chance has placed us, but is always the result of a good conscience, good health, occupation, and freedom in all just pursuits.</p>
-                        <p>Being in control of your life and having realistic expectations about your day-to-day challenges are the keys to stress management, which is perhaps the most important ingredient to living a happy, healthy and rewarding life.</p>
-                        <p><a href="#" class="btn btn-transparent-white btn-capsul">Explore More</a></p>
+                        <p>A ideia surgiu com a percepção de que muitas pessoas ainda utilizam métodos falhos para controlar suas reservas e também com a falta de sistemas voltados para o aluguel de imóveis com baixa complexidade e fácil aprendizado.</p>
+                        <p>O projeto foi desenvolvido por David Spader Ambrózio, na disciplina Projeto integrador, no curso de Sistemas para internet, na Universidade do Vale do Itajaí (Univali), Itajaí, Santa Catarina.</p>
+                        <p><a href="index.php?pagina=login" class="btn btn-transparent-white btn-capsul">Cadastre-se agora!</a></p>
                     </div>
                 </div>
             </div>
