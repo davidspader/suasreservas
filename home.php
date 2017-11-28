23d984d2647eabfd7e76e0a89553f99713d0d157
David Spader <davidspader97@gmail.com>
2017-11-28 03:15:08 -0200

    Adicionando novo template para a página inicial

--- home.php
+++ home.php
@@ -1,146 +1,151 @@
-<!-- Navigation -->
-<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="index.php"><img src="css/imagens/logo/logo-verde.png"></a>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href=" ">
+        <title>Suas Reservas</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="landing-page/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="landing-page/css/custom.css" rel="stylesheet">
+
+        <!-- Helper Styles -->
+        <link href="landing-page/css/loaders.css" rel="stylesheet">
+        <link href="landing-page/css/swiper.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="landing-page/css/animate.min.css">
+        <link rel="stylesheet" href="landing-page/css/nivo-lightbox.css">
+        <link rel="stylesheet" href="landing-page/css/nivo_themes/default/default.css">
+        <!-- Font Awesome Style -->
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="loader loader-bg">
+            <div class="loader-inner ball-clip-rotate-pulse">
+                <div></div>
+                <div></div>
+            </div>
         </div>
 
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" id="cadastro"><strong>Cadastre-se</strong></a>
-                    <ul id="login-dp" class="dropdown-menu">
-                        <li>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <form class="form formularioCadastro" role="form" method="post" action="control/cadastroController.php">
-                                        <div class="form-group">
-                                            <label class="sr-only" for="nomeCadastro">Nome Completo</label>
-                                            <input type="text" class="form-control" id="nomeCadastro" name="nomeCadastro" placeholder="Nome completo" required>
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="sr-only" for="emailCadastro">E-mail</label>
-                                            <input type="text" class="form-control" id="emailCadastro" name="emailCadastro" placeholder="E-mail" required>
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="sr-only" for="cpfCadastro">CPF</label>
-                                            <input type="text" class="form-control cpf" id="cpfCadastro" name="cpfCadastro" placeholder="CPF" required>
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="sr-only" for="senhaCadastro">Senha</label>
-                                            <input type="password" class="form-control" id="senhaCadastro" name="senhaCadastro" placeholder="Senha" required>
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="sr-only" for="telefoneCadastro">Telefone celular</label>
-                                            <input type="text" class="form-control celular" id="telefoneCadastro" name="telefoneCadastro" placeholder="Telefone celular" required>
-                                        </div>
-                                        <div class="form-group">
-                                            <button type="submit" class="btn botao-padrao btn-block" id="botaoCadastro">Cadastrar</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </li>
+        <!-- Top Navigation -->
+        <nav class="navbar navbar-toggleable-md mb-4 top-bar navbar-static-top sps sps--abv">
+            <div class="container">
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse1" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="#"><img src="landing-page/img/logo/logo-verde.png"</span></a>
+                <div class="collapse navbar-collapse" id="navbarCollapse1">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item"> <a class="nav-link" href="index.php?pagina=login">Cadastre-se</a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="index.php?pagina=login&login=1">Login</a> </li>
                     </ul>
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><strong>Login</strong></a>
-                    <ul id="login-dp" class="dropdown-menu">
-                        <li>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <form class="form formularioLogin" role="form" method="post" action="control/loginController.php" id="formularioLogin">
-                                        <input type="hidden" name="req" value="entrar">
-                                        <div class="form-group">
-                                            <label class="sr-only" for="emailLogin">E-mail</label>
-                                            <input type="text" class="form-control" id="emailLogin" name="emailLogin" placeholder="E-mail" required>
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="sr-only" for="senhaLogin">Senha</label>
-                                            <input type="password" class="form-control" id="senhaLogin" name="senhaLogin" placeholder="Senha" required>
-                                        </div>
-                                        <div class="form-group">
-                                            <button type="submit" class="btn botao-padrao btn-block" id="botaoLogin">Entrar</button>
-                                        </div>
-                                    </form>
-                                </div>
+                </div>
+            </div>
+        </nav>
+
+
+        <!-- Swiper Silder
+            ================================================== --> 
+        <!-- Slider main container -->
+        <div class="swiper-container main-slider" id="myCarousel">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide slider-bg-position" style="background:url('landing-page/img/1.jpg')" data-hash="slide1">
+                    <h2>It is health that is real wealth and not pieces of gold and silver</h2>
+                </div>
+                <div class="swiper-slide slider-bg-position" style="background:url('landing-page/img/3.jpg')" data-hash="slide2">
+                    <h2>Happiness is nothing more than good health and a bad memory</h2>
+                </div>
+            </div>
+            <!-- Add Pagination -->
+            <div class="swiper-pagination"></div>
+            <!-- Add Navigation -->
+            <div class="swiper-button-prev"><i class="fa fa-chevron-left"></i></div>
+            <div class="swiper-button-next"><i class="fa fa-chevron-right"></i></div>
+        </div>
+
+        <!-- Benefits
+            ================================================== -->
+        <section class="service-sec" id="benefits">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="heading text-md-center text-xs-center">
+                            <h2><small>Benefits of Exercise</small>To enjoy the glow of good health, you must exercise</h2>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-3 text-sm-center service-block"> <i class="fa fa-plus" aria-hidden="true"></i>
+                                <h3>Better Sleep</h3>
+                                <p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe.</p>
                             </div>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-</nav>
-<header class="masthead">
-    <div class="overlay">
-        <div class="container">
-            <h1 id="bannerTitle">Total controle sobre suas reservas!</h1>
-        </div>
-    </div>
-</header>
+                            <div class="col-md-3 text-sm-center service-block"> <i class="fa fa-leaf" aria-hidden="true"></i>
+                                <h3>Reduces Weight</h3>
+                                <p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe.</p>
+                            </div>
+                            <div class="col-md-3 text-sm-center service-block"> <i class="fa fa-leaf" aria-hidden="true"></i>
+                                <h3>Improves Mood</h3>
+                                <p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe.</p>
+                            </div>
+                            <div class="col-md-3 text-sm-center service-block"> <i class="fa fa-bell" aria-hidden="true"></i>
+                                <h3>Boosts Energy</h3>
+                                <p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row --> 
+            </div>
+        </section>
 
-<div class="geral">
-    
-<section id="iconesDescricao">
-    <div class="container">
-        <div class="row align-items-center text-center">
-            <div class="col-md-4">
-                <div>
-                    <img class="img-fluid" src="css/imagens/icones/icone-casa.png" alt="">
-                </div>
-                <div>
-                    <h2><strong>Imóveis</strong></h2>
-                    <p class="p-10">Adicione e gerencie a quantidade de imóveis que você desejar, o gerenciamento das reservas é feito de forma individual baseado em cada imóvel.</p>
-                </div>
-            </div>
-            <div class="col-md-4 p-5">
-                <div>
-                    <img class="img-fluid" src="css/imagens/icones/icone-intermediador.png">
-                </div>
-                <div>
-                    <h2><strong>Intermediadores</strong></h2>
-                    <p class="p-10">Cadastre intermediadores. Intermediadores são pessoas (físicas ou jurídicas) que auxiliam a locação do imóvel, uma imobiliária por exemplo.</p>
+        <!-- About 
+            ================================================== -->
+        <section class="about-sec parallax-section" id="about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <h2><small>Who We Are</small>About<br>
+                            Our Blog</h2>
+                    </div>
+                    <div class="col-md-4">
+                        <p>To enjoy good health, to bring true happiness to one's family, to bring peace to all, one must first discipline and control one's own mind. If a man can control his mind he can find the way to Enlightenment, and all wisdom and virtue will naturally come to him.</p>
+                        <p>Saving our planet, lifting people out of poverty, advancing economic growth... these are one and the same fight. We must connect the dots between climate change, water scarcity, energy shortages, global health, food security and women's empowerment. Solutions to one problem must be solutions for all.</p>
+                    </div>
+                    <div class="col-md-4">
+                        <p>Our greatest happiness does not depend on the condition of life in which chance has placed us, but is always the result of a good conscience, good health, occupation, and freedom in all just pursuits.</p>
+                        <p>Being in control of your life and having realistic expectations about your day-to-day challenges are the keys to stress management, which is perhaps the most important ingredient to living a happy, healthy and rewarding life.</p>
+                        <p><a href="#" class="btn btn-transparent-white btn-capsul">Explore More</a></p>
+                    </div>
                 </div>
             </div>
-            <div class="col-md-4 p-5">
-                <div>
-                    <img class="img-fluid" src="css/imagens/icones/icone-contrato.png" alt="">
-                </div>
-                <div>
-                    <h2><strong>Contratos</strong></h2>
-                    <p class="p-10">Crie contratos. Utilizando uma reserva é possível gerar um contrato dinâmico com todos os dados sobre a reserva.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+        </section>
 
-<section>
-    <div class="container-fluid mb-15">
-        <div class="text-center">
-            <h2 id="title">Você precisa gerenciar suas reservas? Nós te ajudamos!</h2>
-            <p class="sub">O Suas reservas ajudará a fazer o controle de todas as suas reservas, de forma totalmente online.</p>
-            <div class="col-md-6 centered">
-                <a href="index.php?pagina=login" class="btn btn-lg botao-home btn-block mb-15">Cadastre-se agora! É totalmente grátis</a>
+        <footer>
+            <div class="container">
+                <div class="row copy-footer">
+                    <div class="texto-verde"> &copy;<script type="text/javascript">document.write(new Date().getFullYear());</script> suasreservas.herokuapp.com </div>
+                </div>
             </div>
-        </div>
-    </div>
-</section>
+        </footer>
 
-<footer class="bg-padrao text-center">
-    <div>
-        <p>Todos os diretos reservados &copy;</p>
-    </div>
-</footer>
-</div>
-<script src="js/validacao-formulario/js.validar-formulario-cadastro.js"></script>    
-<script src="js/validacao-formulario/js.validar-formulario-login.js"></script>    
-    
+        <!-- Bootstrap core JavaScript
+            ================================================== --> 
+        <!-- Placed at the end of the document so the pages load faster --> 
+        <script src="landing-page/js/jquery.min.js" ></script> 
+        <script src="landing-page/js/bootstrap.min.js"></script> 
+        <script src="landing-page/js/scrollPosStyler.js"></script> 
+        <script src="landing-page/js/swiper.min.js"></script> 
+        <script src="landing-page/js/isotope.min.js"></script> 
+        <script src="landing-page/js/nivo-lightbox.min.js"></script> 
+        <script src="landing-page/js/wow.min.js"></script> 
+        <script src="landing-page/js/core.js"></script> 
+
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug --> 
+        <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(isset($_SESSION['logado'])){
+if (isset($_SESSION['logado'])) {
     header("Location: dashboard.php");
 }
 include_once './model/Usuario.php';
@@ -21,25 +21,33 @@ $paginas = array(
     )
 );
 $selecionada = isset($paginas[$pagina]) ? $paginas[$pagina] : $paginas['home'];
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title><?php echo $selecionada['titulo']; ?></title>
-        <link rel="icon" href="css/imagens/icones/icone-titulo.png" type="image/x-icon">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link href="bootstrap-jquery/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/home-estilo.css" rel="stylesheet">
-        <link href="css/padrao-estilo.css" rel="stylesheet">
-        <script src="bootstrap-jquery/jquery/jquery.min.js"></script>
-        <script src="bootstrap-jquery/bootstrap/js/bootstrap.min.js"></script>
-        <script src="bootstrap-jquery/jquery/jquery.maskedinput.js"></script>
-        <script src="bootstrap-jquery/jquery/jquery.validate.js"></script>
-        <script src="js/jquery.validate-metodos.js"></script>
-    </head>
-    <body>
-        <div id="conteudo"><?php include_once($selecionada['arquivo']); ?></div>
-    </body>
-    <script src="js/js.mascaras.js"></script>    
-</html>
+
+if ($selecionada['arquivo'] != "home.php") {
+    ?>
+
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <title><?php echo $selecionada['titulo']; ?></title>
+            <link rel="icon" href="css/imagens/icones/icone-titulo.png" type="image/x-icon">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <link href="bootstrap-jquery/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+            <link href="css/home-estilo.css" rel="stylesheet">
+            <link href="css/padrao-estilo.css" rel="stylesheet">
+            <script src="bootstrap-jquery/jquery/jquery.min.js"></script>
+            <script src="bootstrap-jquery/bootstrap/js/bootstrap.min.js"></script>
+            <script src="bootstrap-jquery/jquery/jquery.maskedinput.js"></script>
+            <script src="bootstrap-jquery/jquery/jquery.validate.js"></script>
+            <script src="js/jquery.validate-metodos.js"></script>
+        </head>
+        <body>
+            <div id="conteudo"><?php include_once($selecionada['arquivo']); ?></div>
+        </body>
+        <script src="js/js.mascaras.js"></script>    
+    </html>
+
+<?php
+} else {
+    include_once($selecionada['arquivo']);
+}
